14799717be1b49c273c6b612b1b6abc32aaf0f6d
Huzaifa Ch <huzaifach@gmail.com>
2023-10-13 19:15:53 +0200

    test4-1

--- host/main.tf
+++ host/main.tf
@@ -2,7 +2,7 @@ resource "ssh_resource" "ssh_host" {
 
   host        = "ec2-54-204-157-249.compute-1.amazonaws.com"
   user        = "ubuntu"
-  private_key = file("${var.SSH_KEY}")
+  private_key = var.SSH_KEY //file("${var.SSH_KEY}")
 
   timeout     = "15m"
   retry_delay = "5s"
