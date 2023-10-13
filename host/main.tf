417692978e6ecefbe767585e97466366af05fd1e
Huzaifa Ch <huzaifach@gmail.com>
2023-10-13 17:35:08 +0200

    test

---
+++ host/main.tf
@@ -0,0 +1,23 @@
+resource "ssh_resource" "ssh_host" {
+
+  host        = "ec2-54-204-157-249.compute-1.amazonaws.com"
+  user        = "ubuntu"
+  private_key = file("eksctl-vm-key.pem")
+
+  timeout     = "15m"
+  retry_delay = "5s"
+
+  commands = [
+    "sudo snap install docker",
+    "sudo docker run -d --name alpine-container alpine:latest sleep infinity"
+  ]
+}
+
+terraform {
+  required_providers {
+    ssh = {
+      source  = "loafoe/ssh"
+      version = "2.6.0"
+    }
+  }
+}
